3bd450cb594d5a241e68d7c8f1328658a2bab570
Md Shahbaz Alam <sa0418600@gmail.com>
2024-11-04 14:20:12 +0530

    Add required Attributes to Input and Textarea

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,7 +24,8 @@ const Home = () => {
     }
   }, [pasteId]);
 
-  function createPaste() {
+  function submitHandler(event) {
+    event.preventDefault();  
     const paste = {
       title: title,
       content: value,
@@ -48,49 +49,54 @@ const Home = () => {
 
   return (
     <div className="w-full h-full py-10 max-w-[1200px] mx-auto px-5 ">
-      <div className="w-full flex sm:flex-nowrap flex-wrap-reverse gap-x-4 justify-between items-center">
-        <input
-          className="border border-blue-500 rounded-md text-black p-2  md:w-[80%] sm:w-[70%] w-[100%] focus:outline-none focus:ring-2 focus:ring-blue-300 transition"
-          placeholder="Title"
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
+      <form onSubmit={submitHandler}>
+        <div className="w-full flex sm:flex-nowrap flex-wrap-reverse gap-x-4 justify-between items-center">
+          <input
+            required
+            className="border border-blue-500 rounded-md text-black p-2  md:w-[80%] sm:w-[70%] w-[100%] focus:outline-none focus:ring-2 focus:ring-blue-300 transition"
+            placeholder="Title"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
 
-        <button
-          onClick={createPaste}
-          className="rounded-lg text-md text-white bg-blue-700 p-2 md:w-[20%] sm:w-[30%] w-[100%] hover:bg-blue-800 focus:ring-2 focus:ring-blue-300 font-medium transition mb-5 sm:mb-0"
-        >
-          {pasteId ? "Update My Paste" : "Create My Paste"}
-        </button>
-      </div>
-
-      {/* Circle and copy button */}
-      <div className="w-full mt-5 flex border-[rgba(128,121,121,0.3)] bg-gray-800 rounded-t-md px-4 py-2">
-        <div className="flex gap-x-[6px] select-none items-center">
-          <div className="w-[13px] h-[13px] rounded-full flex items-center justify-center p-[1px] bg-[rgb(255,95,87)]"></div>
-          <div className="w-[13px] h-[13px] rounded-full flex items-center justify-center p-[1px] bg-[rgb(254,188,46)]"></div>
-          <div className="w-[13px] h-[13px] rounded-full flex items-center justify-center p-[1px] bg-[rgb(45,200,66)]"></div>
+          <button
+            //onClick={createPaste}
+            type="submit"
+            className="rounded-lg text-md text-white bg-blue-700 p-2 md:w-[20%] sm:w-[30%] w-[100%] hover:bg-blue-800 focus:ring-2 focus:ring-blue-300 font-medium transition mb-5 sm:mb-0"
+          >
+            {pasteId ? "Update My Paste" : "Create My Paste"}
+          </button>
         </div>
 
-        <Copy
-          className="text-blue-500 w-[20px] cursor-pointer hover:text-blue-600 transition ml-auto"
-          onClick={() => {
-            navigator.clipboard.writeText(value);
-            toast.success("Copies to Clipboard", {
-              position: "bottom-right",
-            });
-          }}
-        />
-      </div>
+        {/* Circle and copy button */}
+        <div className="w-full mt-5 flex border-[rgba(128,121,121,0.3)] bg-gray-800 rounded-t-md px-4 py-2">
+          <div className="flex gap-x-[6px] select-none items-center">
+            <div className="w-[13px] h-[13px] rounded-full flex items-center justify-center p-[1px] bg-[rgb(255,95,87)]"></div>
+            <div className="w-[13px] h-[13px] rounded-full flex items-center justify-center p-[1px] bg-[rgb(254,188,46)]"></div>
+            <div className="w-[13px] h-[13px] rounded-full flex items-center justify-center p-[1px] bg-[rgb(45,200,66)]"></div>
+          </div>
 
-      <textarea
-        className="border border-t-0 border-gray-800 rounded-md rounded-t-none text-black p-2 w-full outline-none"
-        value={value}
-        placeholder="Write Your Content Here...."
-        rows={19}
-        onChange={(e) => setValue(e.target.value)}
-      />
+          <Copy
+            className="text-blue-500 w-[20px] cursor-pointer hover:text-blue-600 transition ml-auto"
+            onClick={() => {
+              navigator.clipboard.writeText(value);
+              toast.success("Copies to Clipboard", {
+                position: "bottom-right",
+              });
+            }}
+          />
+        </div>
+
+        <textarea
+          required
+          className="border border-t-0 border-gray-800 rounded-md rounded-t-none text-black p-2 w-full outline-none"
+          value={value}
+          placeholder="Write Your Content Here...."
+          rows={19}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </form>
     </div>
   );
 };
